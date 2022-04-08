dd4881ca40ba0b7c8f9c76b3881fe87f67635e52
Your Name <kalaitzidispaul@gmail.com>
2022-04-07 21:58:42 -0400

    added inquirer prompt with choices

--- server.js
+++ server.js
@@ -1,2 +1,35 @@
 const mysql = require('mysql2')
 const inquirer = require('inquirer')
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MYSQL Username
+        user: 'root',
+        // password
+        password: '',
+        database: 'employees'
+    },
+    console.log('Connected to the employees database!')
+)
+
+// Prompt user 
+const promptChoices = () => {
+    inquirer.prompt([
+        {
+            name: 'choices',
+            type: 'list',
+            message: 'Please choose an option',
+            choices: [
+                "View all employees",
+                "View all deparment",
+                "View all roles",
+                "Add a department",
+                "Add a Role",
+                "Add an Employee",
+                "Update Employee Role",
+                "None"
+            ]
+        }
+    ])
+}
